3246b2c0b9c967aeae0b7caaa2b0dfa80431e8af
Sander Vanhove <sander.vanhove@ugent.be>
2018-02-23 16:01:13 +0100

    Further completion of the terminal validator.

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
 # sf-terminal-validator
 Sfvalidator implemented to validate one or a batch of url's
 
-## Goals
- - Beeing able to check 1 url
- - Beeing able to check batches in the form of files or arrays
- 	* With aliasses for urls
- - Color coded output
- - Nicely formatted output
+## Features
+ - Validate 1 or a whole file of URLs
+ - Give aliases to URLs 
+ - Color coded output in tables
+
+## Usage
+For validating one URL it is as simple as:
+
+```
+node index.js <URL> <Optional alias>
+```
+
+For validating a file full of URLs just use:
+
+```
+node index.js --file <File path>
+```
+
+The file itself should have a URL and optional alias on seperate lines, like this:
+
+```
+<URL> <Optional alias>
+<URL> <Optional alias>
+...
+```
--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const tv = require("./bin/terminal-validator.js");
+const args = require('minimist')(process.argv.slice(2));
+const validator = new (require("./bin/terminal-validator.js"))();
 
-var validator = new tv();
-validator.validate_url("https://linked.open.gent/parking");
-validator.validate_url("https://www.pietercolpaert.be");
+//Normal input
+if(args._.length > 0)
+	if(args._.length > 1)
+		validator.validate_url(args._[0], args._[1]);
+	else
+		validator.validate_url(args._[0]);
+
+//File input
+if(args.file){
+	var lineReader = require('readline').createInterface({
+		input: require('fs').createReadStream(args.file)
+	});
+
+	lineReader.on('line', line => {
+		var site_args = line.split(" ");
+		if(site_args.length > 1)
+			validator.validate_url(site_args[0], site_args[1]);
+		else
+			validator.validate_url(site_args[0]);
+	});
+}
